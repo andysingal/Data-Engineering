efe4b8f2c50a53df7f967325a0c5fcd2efe3c537
ankush.singal <andysingal@gmail.com>
2023-03-30 14:13:40 +0530

    --1-- movies_scrapping.py

---
+++ API/movies_scrapping.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+response = requests.get(url)
+website_html = response.text
+soup = BeautifulSoup(website_html, "html.parser")
+
+# print(soup.prettify())
+all_movies  = soup.find_all("h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
